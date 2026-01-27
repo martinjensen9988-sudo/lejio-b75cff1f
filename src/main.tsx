476301ea86a168bcd98fae82d6decb78a279db64
martinjensen9988-sudo <223285990+martinjensen9988-sudo@users.noreply.github.com>
2026-01-27 17:15:16 +0000

    Fix: Import Sentry in main.tsx to resolve ReferenceError on app initialization

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { initSentry } from "./lib/sentry";
+import * as Sentry from "@sentry/react";
 
 // Initialize Sentry for error tracking
 initSentry();
